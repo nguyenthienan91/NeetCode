e5684708bce0286229ba73510a1fe04d1b01b47a
Nguyen Thien An <nguyenthienandzpro@gmail.com>
2025-09-15 22:40:29 +0700

    Top k frequent elements

---
+++ TopKFrequentElements/src/Solution/main.java
@@ -0,0 +1,43 @@
+
+package Solution;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+
+
+
+public class main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3,3,3};
+        int k = 2;
+        int[] res = topKFrequent(nums, k);
+        System.out.println(Arrays.toString(res));
+        
+    }
+    
+    
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        
+        List<int[]> arr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            arr.add(new int[] {entry.getValue(), entry.getKey()});
+            
+        }
+        arr.sort((a,b) -> b[0] - a[0]);
+        
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++){
+            res[i] = arr.get(i)[1];
+        }
+        
+        return res;
+
+    }
+}
